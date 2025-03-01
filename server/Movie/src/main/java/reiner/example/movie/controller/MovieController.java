fd69ea069a10eeff2a83e5aa5a1ea830bc3632b9
Youwei <liyouwei654321@gmail.com>
2025-02-28 21:48:32 -0800

    commit Search for a movie by keyword API

--- server/Movie/src/main/java/reiner/example/movie/controller/MovieController.java
+++ server/Movie/src/main/java/reiner/example/movie/controller/MovieController.java
@@ -4,15 +4,14 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reiner.example.movie.model.Movie;
 import reiner.example.movie.service.MovieService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @ClassName MovieController
@@ -27,20 +26,41 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    /**
+     * Get all movies
+     * @return
+     */
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
 
     }
-//    @GetMapping
-//    public List<Movie> getAllMovies2() {
-//        return  movieService.allMovies();
-//    }
+
+    /**
+     * Search for a movie by keyword
+     * @param keyword
+     * @return
+     */
+    @GetMapping("/search")
+    public ResponseEntity<List<Movie>> searchMovie(@RequestParam String keyword) {
+        List<Movie> filteredMovies = movieService.allMovies().stream()
+                .filter(movie -> movie.getTitle().toLowerCase().contains(keyword.toLowerCase()))
+                .collect(Collectors.toList());
+        return filteredMovies.isEmpty() ?  ResponseEntity.notFound().build() : ResponseEntity.ok(filteredMovies);
 
 
+    }
+
+    /**
+     * Get a movie by id
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id) {
         return new ResponseEntity<Optional<Movie>>(movieService.getMovieById(id), HttpStatus.OK);
     }
 
+
+    public ResponseEntity<Optional<Movie>> get;
 }
