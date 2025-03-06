e78e9b3b9e2d97aac88e2cefa7620e6148bb3e8d
Youwei <liyouwei654321@gmail.com>
2025-03-06 10:08:01 -0800

    Added axios by ngrok mapping

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL:'https://9c96-103-106-239-104.ap.ngrok.io/',
-    baseURL:'http://localhost:8080',  
+    baseURL: 'https://c09a-38-34-84-35.ngrok-free.app'
+,
+    // baseURL:'http://localhost:8080',  
     headers: {"ngrok-skip-browser-warning": "true"}
 });
