0c80c87be0465854c803b02e825b76c3fdb9fd04
Youwei <liyouwei654321@gmail.com>
2025-03-23 10:22:21 -0700

    fix CORS add Vercel domin name

--- server/Movie/src/main/java/reiner/example/config/CorsConfig.java
+++ server/Movie/src/main/java/reiner/example/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000",
+                                "https://movie-site-kngb343pm-youweis-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
