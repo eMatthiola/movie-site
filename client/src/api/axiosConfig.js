9ee8db7b9274bbe1e08c18dc660e4516bd68be4f
Youwei <liyouwei654321@gmail.com>
2025-03-23 09:56:57 -0700

    axios URL to Vercel

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export default axios.create({
-    baseURL: ' https://cb3a-38-34-84-35.ngrok-free.app',
-    // baseURL:'http://localhost:8080',  
+    //baseURL: ' https://cb3a-38-34-84-35.ngrok-free.app',
+    // baseURL:'http://localhost:8080', 
+    baseURL: baseURL,
     headers: {"ngrok-skip-browser-warning": "true"}
 });
