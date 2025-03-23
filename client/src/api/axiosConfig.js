189ea871953cd69b059e8fdc05208129b569facc
Youwei <liyouwei654321@gmail.com>
2025-03-23 09:34:23 -0700

    axios URL

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://c09a-38-34-84-35.ngrok-free.app'
-,
+    baseURL: ' https://cb3a-38-34-84-35.ngrok-free.app',
     // baseURL:'http://localhost:8080',  
     headers: {"ngrok-skip-browser-warning": "true"}
 });
